003d8d83a37adc619c5dea4cc7c084a3a5ecd880
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2018-07-29 19:06:01 +0900

    Use GitHub API to create commit/PR instead of Git lib

---
+++ github.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"io/ioutil"
+	"log"
+	"strings"
+	"time"
+)
+
+type GitHubService struct {
+	Repository GitHubRepository
+	Author     CommitAuthor
+	Client     *github.Client
+}
+
+type GitHubRepository struct {
+	Owner string
+	Name  string
+}
+
+type CommitAuthor struct {
+	Name  string
+	Email string
+}
+
+type PullRequest struct {
+	TargetBranch  string
+	CommitBranch  string
+	FileContent   []byte
+	FilePath      string
+	Title         string
+	CommitMessage string
+}
+
+func NewGitHubService(token string, repo GitHubRepository, author CommitAuthor) *GitHubService {
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	return &GitHubService{
+		Repository: repo,
+		Author:     author,
+		Client:     client,
+	}
+}
+
+func (g *GitHubService) DefaultBranch() (*string, error) {
+	repo, _, err := g.Client.Repositories.Get(context.Background(), g.Repository.Owner, g.Repository.Name)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch GitHub repository: %s", err)
+	}
+	defaultBranch := repo.GetDefaultBranch()
+	return &defaultBranch, nil
+}
+
+func (g *GitHubService) Branches() ([]github.Branch, error) {
+	branches, _, err := g.Client.Repositories.ListBranches(context.Background(), g.Repository.Owner, g.Repository.Name, &github.ListOptions{
+		PerPage: 100,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to fetch GitHub branches: %s", err)
+	}
+	return filter(branches, func(branch github.Branch) bool { return !strings.Contains(branch.GetName(), "/") }), nil
+}
+
+func (g *GitHubService) File(branch, path string) ([]byte, error) {
+	file, err := g.Client.Repositories.DownloadContents(context.Background(), g.Repository.Owner, g.Repository.Name, path, &github.RepositoryContentGetOptions{
+		Ref: branch,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to download file from GitHub: %s", err)
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to download file from GitHub: %s", err)
+	}
+
+	return bytes, nil
+}
+
+func filter(vs []*github.Branch, f func(github.Branch) bool) []github.Branch {
+	vsf := make([]github.Branch, 0)
+	for _, v := range vs {
+		if f(*v) {
+			vsf = append(vsf, *v)
+		}
+	}
+	return vsf
+}
+
+// CreateBranch returns the commit branch reference object if it exists or creates it
+// from the base branch before returning it.
+func (g *GitHubService) CreateBranch(from, to string) (ref *github.Reference, err error) {
+	if ref, _, err = g.Client.Git.GetRef(context.Background(), g.Repository.Owner, g.Repository.Name, fmt.Sprintf("refs/heads/%s", to)); err == nil {
+		return ref, nil
+	}
+
+	var baseRef *github.Reference
+	if baseRef, _, err = g.Client.Git.GetRef(context.Background(), g.Repository.Owner, g.Repository.Name, fmt.Sprintf("refs/heads/%s", from)); err != nil {
+		return nil, err
+	}
+	newRef := &github.Reference{Ref: github.String(fmt.Sprintf("refs/heads/%s", to)), Object: &github.GitObject{SHA: baseRef.Object.SHA}}
+	ref, _, err = g.Client.Git.CreateRef(context.Background(), g.Repository.Owner, g.Repository.Name, newRef)
+	return ref, err
+}
+
+// GetTree generates the tree to commit based on the given files and the commit
+// of the ref you got in getRef.
+func (g *GitHubService) CreateTree(ref *github.Reference, content []byte, path string) (tree *github.Tree, err error) {
+	entries := []github.TreeEntry{}
+	entries = append(entries, github.TreeEntry{Path: github.String(path), Type: github.String("blob"), Content: github.String(string(content)), Mode: github.String("100644")})
+	tree, _, err = g.Client.Git.CreateTree(context.Background(), g.Repository.Owner, g.Repository.Name, *ref.Object.SHA, entries)
+	return tree, err
+}
+
+// PushCommit creates the commit in the given reference using the given tree.
+func (g *GitHubService) PushCommit(ref *github.Reference, tree *github.Tree, commitMessage string) (err error) {
+	// Get the parent commit to attach the commit to.
+	parent, _, err := g.Client.Repositories.GetCommit(context.Background(), g.Repository.Owner, g.Repository.Name, *ref.Object.SHA)
+	if err != nil {
+		return err
+	}
+	// This is not always populated, but is needed.
+	parent.Commit.SHA = parent.SHA
+
+	// Create the commit using the tree.
+	date := time.Now()
+	author := &github.CommitAuthor{Date: &date, Name: &g.Author.Name, Email: &g.Author.Email}
+	commit := &github.Commit{Author: author, Message: &commitMessage, Tree: tree, Parents: []github.Commit{*parent.Commit}}
+	newCommit, _, err := g.Client.Git.CreateCommit(context.Background(), g.Repository.Owner, g.Repository.Name, commit)
+	if err != nil {
+		return err
+	}
+
+	// Attach the commit to the master branch.
+	ref.Object.SHA = newCommit.SHA
+	_, _, err = g.Client.Git.UpdateRef(context.Background(), g.Repository.Owner, g.Repository.Name, ref, false)
+	return err
+}
+
+// CreatePR creates a pull request. Based on: https://godoc.org/github.com/google/go-github/github#example-PullRequestsService-Create
+func (g *GitHubService) CreatePullRequest(targetBranch, commitBranch, title, description string) (err error) {
+	branch := fmt.Sprintf("%s:%s", g.Repository.Owner, commitBranch)
+
+	newPR := &github.NewPullRequest{
+		Title:               &title,
+		Head:                &branch,
+		Base:                &targetBranch,
+		Body:                &description,
+		MaintainerCanModify: github.Bool(true),
+	}
+
+	pr, _, err := g.Client.PullRequests.Create(context.Background(), g.Repository.Owner, g.Repository.Name, newPR)
+	if err != nil {
+		return err
+	}
+
+	sugar.Infof("PR created: %s\n", pr.GetHTMLURL())
+	return nil
+}
+
+func (g *GitHubService) PushPullRequest(pullRequest PullRequest) error {
+	ref, err := g.CreateBranch(pullRequest.TargetBranch, pullRequest.CommitBranch)
+	if err != nil {
+		sugar.Errorf("Unable to get/create the commit reference: %s\n", err)
+		return err
+	}
+	if ref == nil {
+		sugar.Errorf("No error where returned but the reference is nil")
+		return err
+	}
+
+	tree, err := g.CreateTree(ref, pullRequest.FileContent, pullRequest.FilePath)
+	if err != nil {
+		sugar.Errorf("Unable to create the tree based on the provided files: %s\n", err)
+		return err
+	}
+
+	if err := g.PushCommit(ref, tree, pullRequest.CommitMessage); err != nil {
+		sugar.Errorf("Unable to create the commit: %s\n", err)
+		return err
+	}
+
+	if err := g.CreatePullRequest(pullRequest.TargetBranch, pullRequest.CommitBranch, pullRequest.Title, pullRequest.CommitMessage); err != nil {
+		log.Fatalf("Error while creating the pull request: %s", err)
+		return err
+	}
+
+	return nil
+}
---
+++ interaction.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/nlopes/slack"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type interactionHandler struct {
+	slackClient       *slack.Client
+	verificationToken string
+}
+
+func (h interactionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		sugar.Errorf("Invalid method: %s", r.Method)
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	buf, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		sugar.Errorf("Failed to read request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	jsonStr, err := url.QueryUnescape(string(buf)[8:])
+	if err != nil {
+		sugar.Errorf("Failed to un-escape request body: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	var message slack.AttachmentActionCallback
+	if err := json.Unmarshal([]byte(jsonStr), &message); err != nil {
+		sugar.Errorf("Failed to decode json message from slack: %s", jsonStr)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if message.Token != h.verificationToken {
+		sugar.Errorf("Invalid token: %s", message.Token)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	action := message.Actions[0]
+	parameters := NewBuildParameters(action.Value)
+	if action.Value == "" {
+		parameters = NewBuildParameters(action.SelectedOptions[0].Value)
+	}
+	switch action.Name {
+	case actionBranch:
+		file, err := service.File(parameters.Branch, "Folio/Info.plist")
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+
+		infoPlist, err := NewInfoPlist(file)
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		tempFile, err := ioutil.TempFile("", "applebot-")
+
+		bytes, err := infoPlist.serialized()
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		tempFile.Write(bytes)
+
+		currentVersion := infoPlist.VersionString()
+		currentBuildNumber := infoPlist.BuildNumberString()
+
+		nextPatch, err := infoPlist.NextPatch()
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		nextMinor, err := infoPlist.NextMinor()
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		nextMajor, err := infoPlist.NextMajor()
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		nextBuildNumber, err := infoPlist.NextBuildNumber()
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+
+		buildParameters := BuildParameters{
+			Branch:             parameters.Branch,
+			Version:            "",
+			BuildNumber:        "",
+			CurrentVersion:     currentVersion,
+			CurrentBuildNumber: currentBuildNumber,
+			NextPatch:          nextPatch,
+			NextMinor:          nextMinor,
+			NextMajor:          nextMajor,
+			NextBuildNumber:    nextBuildNumber,
+			InfoPlist:          tempFile.Name(),
+		}
+
+		responseAction(w, message.OriginalMessage, fmt.Sprintf("Branch: `%s`\nVersion:", parameters.Branch), versionOptions(buildParameters))
+	case actionVersion:
+		responseAction(w, message.OriginalMessage, fmt.Sprintf("Branch: `%s`\nVersion: `%s`", parameters.Branch, parameters.Version), buildNumberOptions(parameters))
+	case actionBuildNumber:
+		currentVersion := fmt.Sprintf("%s (%s)", parameters.CurrentVersion, parameters.CurrentBuildNumber)
+		nextVersion := fmt.Sprintf("%s (%s)", parameters.Version, parameters.BuildNumber)
+		responseAction(w, message.OriginalMessage, fmt.Sprintf("Branch: `%s`\nCurrent Version: `%s`\nNext Version: `%s`", parameters.Branch, currentVersion, nextVersion), okCancelOptions(parameters))
+	case actionRun:
+		bytes, err := ioutil.ReadFile(parameters.InfoPlist)
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+		infoPlist, err := NewInfoPlist(bytes)
+		if err != nil {
+			responseError(w, message.OriginalMessage, "Error occurred.", fmt.Sprintf("%s", err))
+			return
+		}
+
+		responseMessage(w, message.OriginalMessage, "Running ...", "")
+
+		go func() {
+			infoPlist.SetVersion(parameters.Version, parameters.BuildNumber)
+
+			bytes, _ := infoPlist.serialized()
+
+			timestamp := strconv.FormatInt(time.Now().Unix(), 10)
+			commitBranch := fmt.Sprintf("_release/%s-%s-%s", parameters.Version, parameters.BuildNumber, timestamp)
+			title := fmt.Sprintf("Release %s (%s)", parameters.Version, parameters.BuildNumber)
+			commitMessage := title
+
+			service.PushPullRequest(PullRequest{
+				TargetBranch:  parameters.Branch,
+				CommitBranch:  commitBranch,
+				FileContent:   bytes,
+				FilePath:      "Folio/Info.plist",
+				Title:         title,
+				CommitMessage: commitMessage,
+			})
+
+			sugar.Infof("Releasing `%s (%s)`", infoPlist.VersionString(), infoPlist.BuildNumberString())
+		}()
+	case actionCancel:
+		responseMessage(w, message.OriginalMessage, fmt.Sprintf("Operation canceled by '%s'.", message.User.Name), "")
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func responseMessage(w http.ResponseWriter, original slack.Message, title, value string) {
+	original.Attachments[0].Actions = []slack.AttachmentAction{}
+	original.Attachments[0].Fields = []slack.AttachmentField{
+		{
+			Title: title,
+			Value: value,
+			Short: false,
+		},
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&original)
+}
+
+func responseAction(w http.ResponseWriter, original slack.Message, text string, actions []slack.AttachmentAction) {
+	original.Attachments[0].Text = text
+	original.Attachments[0].Actions = actions
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&original)
+}
+
+func responseError(w http.ResponseWriter, original slack.Message, title, value string) {
+	original.Attachments[0].Actions = []slack.AttachmentAction{}
+	original.Attachments[0].Fields = []slack.AttachmentField{
+		{
+			Title: title,
+			Value: value,
+			Short: false,
+		},
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(&original)
+}
+
+func versionOptions(parameters BuildParameters) []slack.AttachmentAction {
+	parameters.Version = parameters.CurrentVersion
+	currentVersionAction := slack.AttachmentAction{
+		Name:  actionVersion,
+		Text:  parameters.Version,
+		Value: parameters.string(),
+		Type:  "button",
+		Style: "primary",
+	}
+
+	parameters.Version = parameters.NextPatch
+	patchVersionAction := slack.AttachmentAction{
+		Name:  actionVersion,
+		Text:  parameters.Version,
+		Value: parameters.string(),
+		Type:  "button",
+	}
+
+	parameters.Version = parameters.NextMinor
+	minorVersionAction := slack.AttachmentAction{
+		Name:  actionVersion,
+		Text:  parameters.Version,
+		Value: parameters.string(),
+		Type:  "button",
+	}
+
+	parameters.Version = parameters.NextMajor
+	majorVersionAction := slack.AttachmentAction{
+		Name:  actionVersion,
+		Text:  parameters.Version,
+		Value: parameters.string(),
+		Type:  "button",
+	}
+	actions := []slack.AttachmentAction{
+		currentVersionAction,
+		patchVersionAction,
+		minorVersionAction,
+		majorVersionAction,
+	}
+	return actions
+}
+
+func buildNumberOptions(parameters BuildParameters) []slack.AttachmentAction {
+	parameters.BuildNumber = parameters.NextBuildNumber
+	currentBuildNumberAction := slack.AttachmentAction{
+		Name:  actionBuildNumber,
+		Text:  parameters.BuildNumber,
+		Value: parameters.string(),
+		Type:  "button",
+		Style: "primary",
+	}
+
+	nextBuildNumber := parameters.NextBuildNumber
+	number, _ := strconv.Atoi(nextBuildNumber)
+	number++
+	var options []slack.AttachmentActionOption
+	for i := number; i <= number+5; i++ {
+		buildNumber := strconv.Itoa(i)
+		parameters.BuildNumber = buildNumber
+		options = append(options, slack.AttachmentActionOption{
+			Text:  buildNumber,
+			Value: parameters.string(),
+		})
+	}
+	actions := []slack.AttachmentAction{
+		currentBuildNumberAction,
+		{
+			Name:    actionBuildNumber,
+			Text:    "Build number",
+			Type:    "select",
+			Options: options,
+		},
+	}
+	return actions
+}
+
+func okCancelOptions(parameters BuildParameters) []slack.AttachmentAction {
+	okAction := slack.AttachmentAction{
+		Name:  actionRun,
+		Text:  "　OK　",
+		Value: parameters.string(),
+		Type:  "button",
+		Style: "primary",
+	}
+	cancelAction := slack.AttachmentAction{
+		Name:  actionCancel,
+		Text:  "Cancel",
+		Value: parameters.string(),
+		Type:  "button",
+	}
+	actions := []slack.AttachmentAction{
+		okAction,
+		cancelAction,
+	}
+	return actions
+}
